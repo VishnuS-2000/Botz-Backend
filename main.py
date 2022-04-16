1bd172bfd1ca20c4dc5dc0ce39c89f97b5aba31c
Vishnu S <vishnusatish2000@gmail.com>
2022-04-16 17:29:33 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,136 @@
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
+
+import numpy
+import tflearn
+import tensorflow
+import random
+import json
+import pickle
+import nltk
+nltk.download('punkt')
+
+from fastapi.middleware.cors import CORSMiddleware
+
+
+
+
+
+from tensorflow.python.framework import ops
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app=FastAPI()
+
+origins=["http://localhost:3000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class TrainingData(BaseModel):
+    data:dict
+    query: str
+
+@app.post("/predict")
+def train_model(train_data:TrainingData):
+    model = []
+    words = []
+    labels = []
+    docs_x = []
+    docs_y = []
+
+    train_data=train_data.dict()
+    data=train_data["data"]
+
+    print(data)
+    for intent in data["intents"]:
+        for pattern in intent["patterns"]:
+            wrds = nltk.word_tokenize(pattern)
+            words.extend(wrds)
+            docs_x.append(wrds)
+            docs_y.append(intent["tag"])
+
+        if intent["tag"] not in labels:
+            labels.append(intent["tag"])
+
+    words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+    words = sorted(list(set(words)))
+
+    labels = sorted(labels)
+
+    training = []
+    output = []
+
+    out_empty = [0 for _ in range(len(labels))]
+
+    for x, doc in enumerate(docs_x):
+        bag = []
+
+        wrds = [stemmer.stem(w.lower()) for w in doc]
+
+        for w in words:
+            if w in wrds:
+                bag.append(1)
+            else:
+                bag.append(0)
+
+        output_row = out_empty[:]
+        output_row[labels.index(docs_y[x])] = 1
+
+        training.append(bag)
+        output.append(output_row)
+
+
+    training = numpy.array(training)
+    output = numpy.array(output)
+    print(output)
+
+
+
+
+    ops.reset_default_graph()
+    net = tflearn.input_data(shape=[None, len(training[0])])
+    net = tflearn.fully_connected(net, 8)
+    net = tflearn.fully_connected(net, 8)
+    net = tflearn.fully_connected(net, len(output[0]), activation='softmax')
+    net = tflearn.regression(net)
+
+    model = tflearn.DNN(net)
+
+    model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+
+
+
+
+    def bag_of_words(s, words):
+        bag = [0 for _ in range(len(words))]
+
+        s_words = nltk.word_tokenize(s)
+        s_words = [stemmer.stem(word.lower()) for word in s_words]
+
+        for se in s_words:
+            for i, w in enumerate(words):
+                if w == se:
+                    bag[i] = 1
+
+        return numpy.array(bag)
+
+
+
+    results = model.predict([bag_of_words(train_data['query'], words)])
+    results_index = numpy.argmax(results)
+    tag = labels[results_index]
+
+    for tg in data["intents"]:
+        if tg['tag'] == tag:
+            responses = tg['responses']
+
+    return {"reply":random.choice(responses)}
+
