af1b57a1667a0e8977f37101619a314af4542928
Vishnu S <63354441+VishnuS-2000@users.noreply.github.com>
2022-04-17 09:07:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -24,15 +24,7 @@ from pydantic import BaseModel
 
 app=FastAPI()
 
-origins=["http://localhost:3000"]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 class TrainingData(BaseModel):
     data:dict
